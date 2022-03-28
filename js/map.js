372f96040ba6321c31ce8938689393103c2fa661
carlo alberto burato <carloalberto.burato@gmail.com>
2022-03-28 12:24:57 +0200

    Add stations features

--- js/map.js
+++ js/map.js
@@ -1,36 +1,30 @@
-export function mapbox() {
+export async function mapbox() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiY2liaWV0aWNpIiwiYSI6ImNrenM1Z2gwdjZ6aHUydm16dzBldXJmcjIifQ.ugFvlWIvOLmyqr7v__e_Ug';
     const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: [10.75, 59.91], // starting position [lng, lat]
-    zoom: 12 // starting zoom
+    zoom: 14 // starting zoom
+    });
+
+    const bikesStations = await getStations();
+
+    const featuresBikes = bikesStations.map(station => {
+        return {
+            type: 'Feature',
+            properties: {
+                message: station.title
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [station.center.longitude, station.center.latitude]
+            }
+        }
     });
 
     const geojson = {
             'type': 'FeatureCollection',
-            'features': [
-                {
-                    'type': 'Feature',
-                    'properties': {
-                        'message': 'Station 1 addressen'
-                    },
-                    'geometry': {
-                        'type': 'Point',
-                        'coordinates': [10.75, 59.91]
-                    }
-                },
-                {
-                    'type': 'Feature',
-                    'properties': {
-                        'message': 'Station 2 addressen'
-                    },
-                    'geometry': {
-                        'type': 'Point',
-                        'coordinates': [10.72, 59.914]
-                    }
-                },
-            ]
+            'features': featuresBikes
     };
 
     // Add markers to the map.
@@ -48,4 +42,18 @@ export function mapbox() {
         .setLngLat(marker.geometry.coordinates)
         .addTo(map);
     };
+};
+
+async function getStations() {
+    const url = `https://api.allorigins.win/get?url=${encodeURIComponent('https://data-legacy.urbansharing.com/legacy-api/stations.json')}`;
+    //const url = 'https://data-legacy.urbansharing.com/legacy-api/stations.json';
+    
+    try {
+        const response = await fetch(url); // fetch er en promise
+        const data = await response.json(); // json er en promise
+        const stations = JSON.parse(data.contents).stations;
+        return stations;
+    } catch (error) {
+        return 'noen gikk galt!'
+    }
 };
